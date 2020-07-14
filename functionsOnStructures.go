522d1d3fdb9fa0a9960d03bd0a1c390b2f65679a
vahid-heidaripour <v.heidaripour@gmail.com>
2020-07-15 00:19:10 +0430

    misc :)

---
+++ functionsOnStructures.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Saiyan struct {
+	Name string
+	Power int
+}
+
+func main() {
+	goku := &Saiyan{"Goku", 9001}
+	goku.Super()
+	fmt.Println(goku.Power)
+}
+
+func (s *Saiyan) Super() {
+	s.Power += 10000
+}
